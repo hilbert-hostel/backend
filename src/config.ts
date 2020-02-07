121466c6bf536fc81cc9db9dee79026793b06bb5
Watcharin Kriengwatana <him10424@gmail.com>
2020-02-07 10:06:13 +0700

    style: add prettier

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 export default {
-  PORT: process.env.PORT as string,
-  NODE_ENV: process.env.NODE_ENV as string
-};
+    PORT: process.env.PORT as string,
+    NODE_ENV: process.env.NODE_ENV as string
+}
