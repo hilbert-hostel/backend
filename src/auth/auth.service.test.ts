20a5e6102706af40f623e1ac34ae835c67da158d
Watcharin Kriengwatana <him10424@gmail.com>
2020-02-13 14:11:55 +0700

    test: auth service

---
+++ src/auth/auth.service.test.ts
@@ -0,0 +1,80 @@
+import { hash } from 'bcryptjs'
+import { IUserRepository } from '../user/user.repository'
+import { RegisterInput } from './auth.interface'
+import { AuthService } from './auth.service'
+import { IJwtService } from './jwt.service'
+
+describe('Auth Service', () => {
+    const userRepository: IUserRepository = {
+        async create(input) {
+            return {
+                id: '1234',
+                ...input
+            }
+        },
+        async findOne({ email }) {
+            const user = {
+                id: '1234',
+                email: 'email',
+                password: await hash('password', 10),
+                firstname: 'asd',
+                lastname: 'asd',
+                phone: 'asd',
+                address: 'zxc'
+            }
+            return email === 'email' ? user : undefined
+        }
+    }
+    const jwtService: IJwtService = {
+        async generateToken() {
+            return 'token'
+        },
+        async verifyToken() {
+            return {} as any
+        }
+    }
+    const authService = new AuthService({ userRepository, jwtService } as any)
+    test('register', async () => {
+        const input: RegisterInput = {
+            email: 'email',
+            password: 'password',
+            firstname: 'asd',
+            lastname: 'asd',
+            phone: 'asd',
+            address: 'zxc'
+        }
+        const { user } = await authService.registerUser(input)
+        expect(user.password).toEqual(expect.any(String))
+        expect(user.password).not.toEqual(input.password)
+        expect(user).toEqual({
+            ...input,
+            id: expect.any(String),
+            password: expect.any(String)
+        })
+    })
+    describe('login', () => {
+        expect.assertions(3)
+        test('wrong email', () => {
+            const input = {
+                email: 'eyy',
+                password: 'password'
+            }
+            expect(authService.login(input)).rejects.toThrow()
+        })
+        test('wrong password', () => {
+            const input = {
+                email: 'email',
+                password: 'asd'
+            }
+            expect(authService.login(input)).rejects.toThrow()
+        })
+        test('correct', async () => {
+            const input = {
+                email: 'email',
+                password: 'password'
+            }
+            const result = await authService.login(input)
+            expect(result).toBeDefined()
+        })
+    })
+})
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -3,7 +3,7 @@ import { CreateUser } from './user.interface'
 
 export interface IUserRepository {
     create(data: CreateUser): Promise<User>
-    findOne(condition: Partial<User>): Promise<User>
+    findOne(condition: Partial<User>): Promise<User | undefined>
 }
 export class UserRepository implements IUserRepository {
     create(data: CreateUser) {
