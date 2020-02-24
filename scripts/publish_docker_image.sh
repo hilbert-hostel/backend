93a1f748f5786e74aff04f9d7299839628f962ab
Rachata <primepianpucktr@gmail.com>
2020-02-24 15:14:44 +0700

    build(ci): add env var DOCKER_REPO

--- scripts/publish_docker_image.sh
+++ scripts/publish_docker_image.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
-docker build -t hilbert-backend .
+docker build -t $DOKCER_REPO .
                         
 if [ $TRAVIS_BRANCH == "master" ]
 then
-    docker tag hilbert-backend $DOCKER_USER/hilbert-backend:latest
-    docker push $DOCKER_USER/hilbert-backend:latest
+    docker tag $DOKCER_REPO $DOCKER_USER/$DOKCER_REPO:latest
+    docker push $DOCKER_USER/$DOKCER_REPO:latest
 else 
-    docker tag hilbert-backend $DOCKER_USER/hilbert-backend:$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
-    docker push $DOCKER_USER/hilbert-backend:$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
-    docker tag hilbert-backend $DOCKER_USER/hilbert-backend:$TRAVIS_BRANCH-latest
-    docker push $DOCKER_USER/hilbert-backend:$TRAVIS_BRANCH-latest
+    docker tag $DOKCER_REPO $DOCKER_USER/$DOKCER_REPO:$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
+    docker push $DOCKER_USER/$DOKCER_REPO:$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
+    docker tag $DOKCER_REPO $DOCKER_USER/$DOKCER_REPO:$TRAVIS_BRANCH-latest
+    docker push $DOCKER_USER/$DOKCER_REPO:$TRAVIS_BRANCH-latest
 fi
