33532d88ca7754cc79f96fa0f8f601e3eb305e1a
Rachata <primepianpucktr@gmail.com>
2020-02-25 19:14:38 +0700

    build(ci): fix typo for DOCKER_REPO

--- scripts/publish_docker_image.sh
+++ scripts/publish_docker_image.sh
@@ -2,15 +2,15 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
 branch=${TRAVIS_BRANCH//\//-}
-docker build -t $DOKCER_REPO .
+docker build -t $DOCKER_REPO .
                         
 if [ $TRAVIS_BRANCH == "master" ]
 then
-    docker tag $DOKCER_REPO $DOCKER_USER/$DOKCER_REPO:latest
-    docker push $DOCKER_USER/$DOKCER_REPO:latest
+    docker tag $DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:latest
+    docker push $DOCKER_USER/$DOCKER_REPO:latest
 else 
-    docker tag $DOKCER_REPO $DOCKER_USER/$DOKCER_REPO:$branch-$TRAVIS_BUILD_NUMBER
-    docker push $DOCKER_USER/$DOKCER_REPO:$branch-$TRAVIS_BUILD_NUMBER
-    docker tag $DOKCER_REPO $DOCKER_USER/$DOKCER_REPO:$branch-latest
-    docker push $DOCKER_USER/$DOKCER_REPO:$branch-latest
+    docker tag $DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:$branch-$TRAVIS_BUILD_NUMBER
+    docker push $DOCKER_USER/$DOCKER_REPO:$branch-$TRAVIS_BUILD_NUMBER
+    docker tag $DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:$branch-latest
+    docker push $DOCKER_USER/$DOCKER_REPO:$branch-latest
 fi
