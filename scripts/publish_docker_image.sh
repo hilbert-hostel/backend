073b866ff6d7a7016561e98387d2226285868821
Rachata <primepianpucktr@gmail.com>
2020-02-26 09:53:50 +0700

    build(ci): introduce new variable

--- scripts/publish_docker_image.sh
+++ scripts/publish_docker_image.sh
@@ -2,15 +2,16 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
 branch=${TRAVIS_BRANCH//\//-}
+tag_name="$DOCKER_USER/$DOCKER_REPO"
 docker build -t $DOCKER_REPO .
                         
 if [ $TRAVIS_BRANCH == "master" ]
 then
-    docker tag $DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:latest
-    docker push $DOCKER_USER/$DOCKER_REPO:latest
+    docker tag $DOCKER_REPO $tag_name:latest
+    docker push $tag_name:latest
 else 
-    docker tag $DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:$branch-$TRAVIS_BUILD_NUMBER
-    docker push $DOCKER_USER/$DOCKER_REPO:$branch-$TRAVIS_BUILD_NUMBER
-    docker tag $DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:$branch-latest
-    docker push $DOCKER_USER/$DOCKER_REPO:$branch-latest
+    docker tag $DOCKER_REPO $tag_name:$branch-$TRAVIS_BUILD_NUMBER
+    docker push $tag_name:$branch-$TRAVIS_BUILD_NUMBER
+    docker tag $DOCKER_REPO $tag_name:$branch-latest
+    docker push $tag_name:$branch-latest
 fi
