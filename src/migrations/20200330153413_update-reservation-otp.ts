ec58aa35b9d6747e78204e969d69cede5e871899
Watcharin Kriengwatana <him10424@gmail.com>
2020-04-02 23:34:39 +0700

    db: update otp and record entity

--- src/migrations/20200330153413_update-reservation-otp.ts
+++ src/migrations/20200330153413_update-reservation-otp.ts
@@ -4,11 +4,24 @@ export async function up(knex: Knex): Promise<any> {
     await knex.schema.alterTable('reservation', table => {
         table.dropColumn('otp')
     })
+    await knex.schema.alterTable('record', table => {
+        table.dropColumn('reservation_id')
+        table.dropColumn('id')
+    })
+    await knex.schema.alterTable('record', table => {
+        table
+            .string('id')
+            .primary()
+            .references('reservation.id')
+            .onDelete('CASCADE')
+    })
     return await knex.schema.createTable('otp', table => {
         table
             .string('id')
             .primary()
             .references('reservation.id')
+            .onDelete('CASCADE')
+
         table.string('password').notNullable()
         table.string('reference_code').notNullable()
     })
@@ -18,5 +31,15 @@ export async function down(knex: Knex): Promise<any> {
     await knex.schema.dropTable('otp')
     await knex.schema.alterTable('reservation', table => {
         table.string('otp').nullable()
+    })
+    await knex.schema.alterTable('record', table => {
+        table.dropColumn('id')
+    })
+    await knex.schema.alterTable('record', table => {
+        table.uuid('id').primary()
+        table
+            .string('reservation_id')
+            .references('reservation.id')
+            .onDelete('CASCADE')
     })
 }
