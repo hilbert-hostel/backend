0a54397e22fb1acfe9b16be457a444d018ff8af0
Watcharin Kriengwatana <him10424@gmail.com>
2020-04-13 15:33:21 +0700

    feat(checkOut): add check out route

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { AuthRouter } from './auth/auth.router'
 import { CheckInRouter } from './checkIn/checkIn.router'
 import { ReservationRouter } from './reservation/reservation.router'
+import { CheckOutRouter } from './checkOut/checkOut.router'
 
 const router = Router()
 
@@ -13,4 +14,5 @@ router.get('/ping', (req, res) => {
 router.use('/auth', AuthRouter)
 router.use('/reservation', ReservationRouter)
 router.use('/checkIn', CheckInRouter)
+router.use('/checkOut', CheckOutRouter)
 export { router as Router }
