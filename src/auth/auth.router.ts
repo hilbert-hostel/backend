28918151badf32b88b9985cbfcd40c669fe0cd0d
Watcharin Kriengwatana <him10424@gmail.com>
2020-04-14 20:24:07 +0700

    refactor(auth): change verify user route to post

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -3,19 +3,8 @@ import { omit } from 'ramda'
 import { container } from '../container'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { validateBody, validateQuery } from '../middlewares/validate'
-import {
-    LoginInput,
-    LoginPayload,
-    RegisterInput,
-    RegisterPayload,
-    VerifyUserInput
-} from './auth.interface'
-import {
-    checkAvailableValidator,
-    loginValidator,
-    registerValidator,
-    verifyUserValidator
-} from './auth.validation'
+import { LoginInput, LoginPayload, RegisterInput, RegisterPayload, VerifyUserInput } from './auth.interface'
+import { checkAvailableValidator, loginValidator, registerValidator, verifyUserValidator } from './auth.validation'
 
 const router = Router()
 const { authService, jwtService, guestRepository } = container
@@ -45,8 +34,8 @@ router.get('/ping', isAuthenticated, async (req, res) => {
     res.json(omit(['password'], user))
 })
 
-router.get('/verify', validateQuery(verifyUserValidator), async (req, res) => {
-    const { userID, token } = req.query as VerifyUserInput
+router.post('/verify', validateBody(verifyUserValidator), async (req, res) => {
+    const { userID, token } = req.body as VerifyUserInput
     const user = await authService.verifyGuest(userID, token)
     res.send(user)
 })
@@ -71,3 +60,4 @@ router.get(
     }
 )
 export { router as AuthRouter }
+
