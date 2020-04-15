a76b43230a988e9b90fc2037d43fef09d99fb753
Sarun <sarunguy@hotmail.com>
2020-04-15 21:49:33 +0700

    feat: Add file upload

--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -1,3 +1,8 @@
+import { Config } from '../config'
+import { Dependencies } from '../container'
+import { reject } from 'ramda'
+import * as AWS from 'aws-sdk'
+
 export interface IFileService {
     uploadFile(file: any, name: string): Promise<string>
     getFile(name: string): Promise<string>
@@ -5,11 +10,49 @@ export interface IFileService {
 
 // TODO implement real file service with S3
 export class FileService implements IFileService {
-    async uploadFile(file: any, name: string) {
-        return 'random url'
+    private BUCKET_ID: string
+    private BUCKET_SECRET: string
+    private BUCKET_NAME: string
+
+    constructor({ config }: Dependencies<Config>) {
+        this.BUCKET_ID = config.BUCKET_ID
+        this.BUCKET_SECRET = config.BUCKET_SECRET
+        this.BUCKET_NAME = config.BUCKET_NAME
     }
 
-    async getFile(name: string) {
-        return 'random url'
+    s3 = new AWS.S3({
+        accessKeyId: this.BUCKET_ID,
+        secretAccessKey: this.BUCKET_SECRET,
+    })
+
+    uploadFile(file: any, name: string): Promise<any> {
+        const params = {
+            Bucket: this.BUCKET_NAME,
+            Key: name,
+            Body: file,
+        }
+        return new Promise((resolve, reject) => {
+            this.s3.upload(params, function (err: any, data: any) {
+                if (err) {
+                    return reject(err)
+                }
+                return resolve(data.key)
+            })
+        })
+    }
+
+    getFile(name: string): Promise<any> {
+        const params = {
+            Bucket: this.BUCKET_NAME,
+            Key: name,
+        }
+        return new Promise((resolve, reject) => {
+            this.s3.getObject(params, function (err: any, data: any) {
+                if (err) {
+                    return reject(err)
+                }
+                return resolve(data.Body)
+            })
+        })
     }
 }
