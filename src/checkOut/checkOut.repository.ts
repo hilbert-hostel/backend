79472bd24b03cbc0a185c8b36e46044e841cf777
Watcharin Kriengwatana <him10424@gmail.com>
2020-04-25 15:25:18 +0700

    feat(checkOut): generate code for check out

--- src/checkOut/checkOut.repository.ts
+++ src/checkOut/checkOut.repository.ts
@@ -3,6 +3,10 @@ import ReservationModel, { Reservation } from '../models/reservation'
 export interface ICheckOutRepository {
     addCheckOutTime(reservation_id: string, time: Date): Promise<Reservation>
     findReservationById(id: string): Promise<Reservation>
+    findReservationToCheckOut(
+        guest_id: string,
+        check_out: Date
+    ): Promise<Reservation>
 }
 export class CheckOutRepository {
     addCheckOutTime(reservation_id: string, time: Date) {
@@ -12,5 +16,12 @@ export class CheckOutRepository {
     }
     findReservationById(id: string): Promise<Reservation> {
         return ReservationModel.query().findById(id)
+    }
+    async findReservationToCheckOut(guest_id: string, check_out: Date) {
+        const result = await ReservationModel.query().findOne({
+            guest_id,
+            check_out
+        })
+        return result
     }
 }
--- src/checkOut/checkOut.service.ts
+++ src/checkOut/checkOut.service.ts
@@ -5,6 +5,7 @@ import { Reservation } from '../models/reservation'
 import { ICheckOutRepository } from './checkOut.repository'
 export interface ICheckOutService {
     checkOut(reservationID: string, date: Date): Promise<Reservation>
+    getCheckoutCode(guest_id: string, date: Date): Promise<{ code: string }>
 }
 export class CheckOutService implements ICheckOutService {
     checkOutRepository: ICheckOutRepository
@@ -25,5 +26,17 @@ export class CheckOutService implements ICheckOutService {
             throw new BadRequestError('You are not checked in.')
         }
         return this.checkOutRepository.addCheckOutTime(reservationID, date)
+    }
+    async getCheckoutCode(guest_id: string, date: Date) {
+        const reservation = await this.checkOutRepository.findReservationToCheckOut(
+            guest_id,
+            date
+        )
+        if (!reservation) {
+            throw new BadRequestError(
+                'No reservation to check out on this date.'
+            )
+        }
+        return { code: reservation.id }
     }
 }
